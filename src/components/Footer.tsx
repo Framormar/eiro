5947de3c5632c677a43338ef82016184a15edd9f
Paco M <framormar01@gmail.com>
2025-03-16 22:12:54 +0100

    TEST 4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,12 +28,12 @@ export const Footer = () => {
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            / Creado por{" "}
             <SmartLink
               style={{ marginLeft: "-0.125rem" }}
-              href="https://once-ui.com/templates/magic-portfolio"
+              href="https://www.linkedin.com/in/francisco-morales-martinez-a4a4b7242/"
             >
-              Once UI
+              Framormar
             </SmartLink>
           </Text>
         </Text>
